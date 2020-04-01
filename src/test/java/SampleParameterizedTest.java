cd3eb87ce5fcde953865fd47c6902252c96a3d24
Alma0516 <alexwass011@gmail.com>
2020-04-01 18:28:27 +0200

    simple test

--- src/test/java/SampleParameterizedTest.java
+++ src/test/java/SampleParameterizedTest.java
@@ -1,11 +1,20 @@
 import alma.Tm4jExtension;
+import annotation.TestCaseKey;
 import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class SampleParameterizedTest {
+
+    @TestCaseKey(keys = "SCENARIO_1")
+    void testName() {
+        assertTrue(true, "work");
+    }
+
 
     @ParameterizedTest
     @MethodSource("methodSource")
