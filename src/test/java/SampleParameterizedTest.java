163cb964027ebe4fc135a7d28140de7b1cd29f90
Alma0516 <alexwass011@gmail.com>
2020-04-01 18:15:31 +0200

    simple test

--- src/test/java/SampleParameterizedTest.java
+++ src/test/java/SampleParameterizedTest.java
@@ -1,20 +1,20 @@
 import alma.Tm4jExtension;
-import org.junit.Assert;
 import org.junit.jupiter.api.TestReporter;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-public class ManualTestKey {
+public class SampleParameterizedTest {
 
     @ParameterizedTest
     @MethodSource("methodSource")
-    public void forParameterizedTest(String testCaseKey, TestReporter testReporter) {
-        testReporter.publishEntry(Tm4jExtension.TEST_CASE_KEY,testCaseKey);
+    public void forParameterizedTest(String testData, TestReporter testReporter) {
+        testReporter.publishEntry(Tm4jExtension.TEST_CASE_KEY,"SCENARIO_1");
+        System.out.println(testData);
     }
 
     public static Stream<String> methodSource() {
-        return Stream.of( "TEST_PROJECT_PARAM");
+        return Stream.of("arg1","arg2");
     }
 }
